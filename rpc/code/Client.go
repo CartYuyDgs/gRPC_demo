19b52f81b0deb138d04df23bd1998fc3fa16e1b2
于洋 <yy_dgs@126.com>
2020-05-22 23:27:39 +0800

    Rpc实现异步调用

--- rpc/code/Client.go
+++ rpc/code/Client.go
@@ -18,12 +18,17 @@ func main() {
 		log.Fatalf("Error %v", err)
 		return
 	}
+	//同步
+	//err = client.Call("MathUtil.Calculate", num, &resp)
+	//if err != nil {
+	//	log.Fatalf("Error %v", err)
+	//	return
+	//}
 
-	err = client.Call("MathUtil.Calculate", num, &resp)
-	if err != nil {
-		log.Fatalf("Error %v", err)
-		return
-	}
+	//异步
+	asyncCall := client.Go("MathUtil.Calculate", num, &resp, nil)
+	replayDone := <-asyncCall.Done
+	fmt.Println(replayDone)
 
 	fmt.Println(resp)
 }
